e4727a02df018060fdaa5fdfe7f2789b5dc81c29
Shubhamaurya1 <shubhamaurya74@gmail.com>
2026-08-03 14:05:49 +0530

    update Firebase admin configuration to use environment variables and clean up commented code

--- server/config/firebaseAdmin.js
+++ server/config/firebaseAdmin.js
@@ -1,13 +1,33 @@
-import fs from "fs";
+// import fs from "fs";
+// import { initializeApp, cert } from "firebase-admin/app";
+// import { getAuth } from "firebase-admin/auth";
+
+// const serviceAccount = JSON.parse(
+//   fs.readFileSync(new URL("./serviceAccountKey.json", import.meta.url), "utf8"),
+// );
+
+// const app = initializeApp({
+//   credential: cert(serviceAccount),
+// });
+
+// export const auth = getAuth(app);
+
+
+import dotenv from "dotenv";
+dotenv.config();
+
 import { initializeApp, cert } from "firebase-admin/app";
 import { getAuth } from "firebase-admin/auth";
 
-const serviceAccount = JSON.parse(
-  fs.readFileSync(new URL("./serviceAccountKey.json", import.meta.url), "utf8"),
-);
+const serviceAccount = {
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+  privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+};
 
 const app = initializeApp({
   credential: cert(serviceAccount),
 });
 
 export const auth = getAuth(app);
+
