1a310e06c760fb8e238f94837e573448a07be214
Shubhamaurya1 <shubhamaurya74@gmail.com>
2026-08-07 17:49:41 +0530

    VehicleCard styling for improved layout and responsiveness

--- client/src/components/dashboard/VehicleCard.jsx
+++ client/src/components/dashboard/VehicleCard.jsx
@@ -33,15 +33,15 @@ const VehicleCard = ({ image, title, vehicleId }) => {
   return (
     <div
       onClick={handleClick}
-      className="bg-white rounded-3xl shadow-md hover:shadow-xl transition cursor-pointer overflow-hidden"
+      className="bg-white p-3 sm:p-5 rounded-2xl sm:rounded-3xl shadow-md hover:shadow-xl transition cursor-pointer"
     >
       <img
         src={image}
         alt={title}
-        className="w-full h-32 md:h-40 object-contain p-4"
+        className="w-full h-20 sm:h-28 object-cover rounded-lg sm:rounded-2xl"
       />
 
-      <h2 className="text-center font-semibold text-sm sm:text-lg mt-2 mb-4">
+      <h2 className="text-center font-semibold text-sm sm:text-lg mt-2 sm:mt-4 line-clamp-2">
         {title}
       </h2>
     </div>
