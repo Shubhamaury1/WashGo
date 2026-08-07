bf9e130dd6337f864a5b38a6e33e88fc8450ca53
Shubhamaurya1 <shubhamaurya74@gmail.com>
2026-08-07 16:45:17 +0530

    TopHeader layout and improve CustomerDashboard styling

--- client/src/pages/CustomerDashboard/CustomerDashboard.jsx
+++ client/src/pages/CustomerDashboard/CustomerDashboard.jsx
@@ -45,7 +45,9 @@ const CustomerDashboard = () => {
           <Sidebar />
 
           {/* Right Content */}
-          <main className="flex-1 md:ml-[294px] overflow-y-auto pr-2 w-full md:w-auto">
+          {/* <main className="flex-1 md:ml-[294px] overflow-y-auto pr-2 w-full md:w-auto">
+           */}
+          <main className="flex-1 w-full md:w-auto md:ml-[294px] overflow-y-auto pt-4 md:pt-0 px-4 md:px-0 pr-0 md:pr-2">
             <TopHeader />
 
             {/* Stats */}
@@ -64,7 +66,9 @@ const CustomerDashboard = () => {
             {/* Vehicle */}
             <div className="mt-8 md:mt-12">
               <div className="flex justify-between items-center gap-4 mb-6 md:mb-8">
-                <h1 className="text-2xl sm:text-3xl font-bold">Book New Service</h1>
+                <h1 className="text-2xl sm:text-3xl font-bold">
+                  Book New Service
+                </h1>
 
                 <button className="text-blue-600 font-semibold text-sm sm:text-base">
                   View All
@@ -85,7 +89,9 @@ const CustomerDashboard = () => {
             {/* Booking */}
             <div className="mt-8 md:mt-12 pb-10">
               <div className="flex justify-between items-center gap-4 mb-6 md:mb-8">
-                <h1 className="text-2xl sm:text-3xl font-bold">Active Booking</h1>
+                <h1 className="text-2xl sm:text-3xl font-bold">
+                  Active Booking
+                </h1>
 
                 <button className="text-blue-600 font-semibold text-sm sm:text-base">
                   View All
